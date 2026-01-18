f8ffdd8abf6181d8e0a477fc29dfd5c09c320086
Vivek Meena <vivek.meena.ops@gmail.com>
2026-01-18 16:38:40 +0530

    testing with different hosted model

--- day-3/hosted-llm-gemini/generate_dockerfile_gemini.py
+++ day-3/hosted-llm-gemini/generate_dockerfile_gemini.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import os
 
 # Set your API key here
-os.environ["GOOGLE_API_KEY"] = "AIzaSyBCJXod8tAguAg74H1AL8ua0uQvpvG3P_c"
+os.environ["GOOGLE_API_KEY"] = "AIzaSyBCJXod8tAguAg74H1AL8ua0uQvpvG3P"
 
 # Configure the Gemini model
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
